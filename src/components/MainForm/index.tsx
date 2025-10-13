7fd65fef67492d0c998338b7eba79bb296650e1c
Daniel Silva <daniel24maio@gmail.com>
2025-10-13 16:56:08 -0300

    criacao e ajuste de tips

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -17,19 +17,6 @@ export function MainForm() {
     const nextCycle = getNextCycle(state.currentCycle);
     const nextCycleType = getNextCycleType(nextCycle);
 
-    const tipsForWhenActiveTask = {
-        workTime: <span>Foque por {state.config.workTime} minutos</span>,
-        shortBreakTime: <span>Descanse por {state.config.shortBreakTime} minutos</span>,
-        longBreakTime: <span>Pausa longa {state.config.longBreakTime} minutos</span>,
-    };
-
-     const tipsForNoActiveTask = {
-        workTime: <span>Próximo ciclo é de <b>{state.config.workTime} minutos</b></span>,
-        shortBreakTime: <span>Próximo ciclo é de <b>{state.config.shortBreakTime} minutos</b></span>,
-        longBreakTime: <span>Próximo ciclo é descanso longo com <b>{state.config.longBreakTime} minutos</b></span>,
-    };
-
-
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
 
         event.preventDefault();
